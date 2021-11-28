d054b94a1f3775428f992c461fae37d93829fb4a
ltpquang <phuc.quang102@gmail.com>
2021-11-28 10:48:25 +0700

    Brew formula update for wally version v0.1.1

--- Formula/wally.rb
+++ Formula/wally.rb
@@ -5,21 +5,21 @@
 class Wally < Formula
   desc "Randomly change wallpaper using Unsplash Source"
   homepage "https://github.com/ltpquang/wally"
-  version "0.1.0"
+  version "0.1.1"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ltpquang/wally/releases/download/v0.1.0/wally_0.1.0_macOS_arm64.tar.gz"
-      sha256 "070352e52cec1ac51761264d6ce2ebd43293ae663cae481fa57a806c752f071e"
+      url "https://github.com/ltpquang/wally/releases/download/v0.1.1/wally_0.1.1_macOS_arm64.tar.gz"
+      sha256 "5048514e8af6366753f3325dd9d2f42e16dfa2b93a15533dd0037471898aef8f"
 
       def install
         bin.install "wally"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ltpquang/wally/releases/download/v0.1.0/wally_0.1.0_macOS_x86_64.tar.gz"
-      sha256 "58f4085b68279b7eda8cb83978c8ea3391ab5bc8d99fc551a01a3ee0f552e805"
+      url "https://github.com/ltpquang/wally/releases/download/v0.1.1/wally_0.1.1_macOS_x86_64.tar.gz"
+      sha256 "7f090ebea0b2a80c0c35d86ec89b21c25e915dcb7738d1240c17fb217f8cbc85"
 
       def install
         bin.install "wally"
